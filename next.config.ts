eb3b2f52677211da70538e42e7de221dfdff69f8
helpteteacherivs <helpteacher.ivs@gmail.com>
2025-09-29 11:04:09 +0700

    commit & push

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type {NextConfig} from 
-\next\';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: \export\',
-  basePath: \/luvyoga\',
-  assetPrefix: \/luvyoga/\',
+  output: "export",
+  basePath: "/luvyoga",
+  assetPrefix: "/luvyoga/",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
@@ -16,22 +15,22 @@ const nextConfig: NextConfig = {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: \https\',
-        hostname: \placehold.co\',
-        port: \'\',
-        pathname: \/**\',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: \https\',
-        hostname: \images.unsplash.com\',
-        port: \'\',
-        pathname: \/**\',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: \https\',
-        hostname: \picsum.photos\',
-        port: \'\',
-        pathname: \/**\',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
     ],
   }
